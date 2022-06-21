6b3dadf11634dc5c7a51994a4d322e6627643258
eternalgratis <tyaps002@gmail.com>
2022-06-21 09:46:34 +0100

    modify styles for nav-menu

--- main.js
+++ main.js
@@ -10,16 +10,3 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
-
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu  = document.querySelector(".nav-menu");
-
-// hamburger.addEventListener("click", () => {
-//   hamburger.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// })
-
-//  document.querySelectorAll(".nav-link").forEach((n) => n.addEventListener("click", () => {
-//    hamburger.classList.remove("active");
-//    navMenu.classList.remove("active");
-//  }))
